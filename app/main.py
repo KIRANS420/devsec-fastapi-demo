ac39c45c93be91924b56484da06ea953dab286d9
vipularya135 <krishnamvipul@gmail.com>
2025-09-27 23:40:34 +0530

    test: add version field to test workflows

--- app/main.py
+++ app/main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"ok": True, "msg": "hello from devsec-fastapi-demo"}
+    return {"ok": True, "msg": "hello from devsec-fastapi-demo", "version": "1.0.0"}
